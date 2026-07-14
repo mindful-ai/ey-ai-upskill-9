22f58b1f4c0b44e05cb42c11935cc5ed3934a360
mindful-ai <mindfullearningindia@outlook.com>
2026-07-14 11:46:09 +0530

    Add files via upload

---
+++ 05-simple-application/chat-bot.py
@@ -0,0 +1,56 @@
+# Imports
+
+
+# Read API keys
+
+
+# Initialize Groq client
+
+
+# Select model
+
+
+# Chat function
+def chat():
+
+    # Welcome message
+
+   
+    # Conversation history (list)
+
+
+    # Inifinite loop
+    while True:
+    
+        # User input
+
+
+        # Check for the exit condition (exit, quit, end)
+
+
+        # Add user input to conversation history
+
+
+        # Build a prompt using conversation history
+        try:
+
+            # Get the Groq response
+
+
+            # extract the output text
+
+
+            # print the output text
+
+
+            # add the ai message into the conversation history
+
+
+        except Exception as e:
+
+            # Add an exception message
+
+
+# run the chatbot
+if __name__ == "__main__":
+    chat()
---
+++ activities/day-1.md
@@ -0,0 +1,24 @@
+### 1.1 Configuring the VM and Python Environment [90]
+
+- Invoke the VM
+- Test Visual Studio Code
+- Ensure Python 3.12 is installed
+- Install all the libraries
+  - Ref: 00-vm-setup\env-setup\requirements.txt
+
+### 1.2 Solve the following using python [15]
+
+- Input some text from the user
+- Print all the unique characters from the text
+- Example:
+  - input <- mississippi
+  - output -> misp
+- Write a script 
+
+------------------------------------------------------
+15:10 - 15:25 Exercise 1.2
+------------------------------------------------------
+
+------------------------------------------------------
+4:20 - 4:40 Tea Break
+------------------------------------------------------
